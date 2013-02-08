02eadab1d052748cddeba8815aca218bcaeb6874
John Shutt <john.d.shutt@gmail.com>
2013-02-07 18:37:43 -0800

    create home page

---
+++ config/initializers/redcarpet.rb
@@ -0,0 +1,11 @@
+class ActionView::Template
+  class Redcarpet < Handler
+    include Handlers::Compilable
+
+    def compile template
+      ::Redcarpet.new(template.source).to_html.inspect
+    end
+  end
+
+  register_template_handler :md, Redcarpet
+end
