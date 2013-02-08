fc5d05863a5b46ed3112b516cb5a5d510b57406d
John Shutt <john.d.shutt@gmail.com>
2013-02-07 17:46:55 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pickup Art Auction
+
+This is a standalone site to promote [Ones in a Lifetime's](http://onesinalifetime.com) charity auction of "Pickup Art" -- art created entirely out of found or recycled materials.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pickupartauction::Application.config.secret_token = '21ba0b2b980aab02693c0185041f6a2992c5f4320397139fb2103aeed0a324d833b519e504885f90aa9a69596912f117a62bad8e15f05c64fdb0afc129cc759b'
